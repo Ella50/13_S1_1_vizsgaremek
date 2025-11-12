5549ae5bae59ba7d883cb6d351d262b9362f3376
Atkári Ariella <AtkariAriella@pg.local>
2025-11-12 12:52:20 +0100

    20251112_1252
    Alapok feltöltése

--- Projekt/backend/routes/api.php
+++ Projekt/backend/routes/api.php
@@ -2,23 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PlayerController;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::group(['prefix' => ''],function(){
-    Route::apiResource('players',PlayerController::class);
-});
+Route::get('/auth/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route::get('/auth/users', [AuthController::class, 'users']);
+Route::get('/auth/alma', [AuthController::class, 'alma']);
