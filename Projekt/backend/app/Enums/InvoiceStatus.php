fbe70f83cfe4affd0ced03e61f1a7ffd693a72f8
Atkári Ariella <AtkariAriella@pg.local>
2025-11-14 09:17:12 +0100

    20251114_0917
    Adatbázis táblák létrehozása

--- Projekt/backend/app/Enums/InvoiceStatus.php
+++ Projekt/backend/app/Enums/InvoiceStatus.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 enum InvoiceStatus: string
 {
+    case HIANYZIK = 'Hiányzik';
     case GENERATED = 'Generálva';
     case PAID = 'Fizetve';
     case EXPIRED = 'Lejárt';
--- Projekt/talánKelleniFog/0001_01_01_000000_create_user_table.php
+++ Projekt/talánKelleniFog/0001_01_01_000000_create_user_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,9 +10,67 @@ use App\Enums\UserStatus;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('thirdName')->nullable();
+            $table->foreignId('city_id')->constrained('city')->onDelete('cascade');
+            $table->string('address');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('userType', array_column(UserType::cases(), 'value'))->default('Tanuló');
+
+            $table->foreignId('rfidCard_id')->constrained('rfidCard');
+            $table->foreignId('class_id')->nullable()->constrained('class');
+            $table->foreignId('group_id')->nullable()->constrained('group');
+            $table->enum('status', array_column(UserStatus::cases(), 'value'))->default('inactive');
+            $table->boolean('hasDiscount')->default(false);
+            $table->timestamps();
+            $table->rememberToken();
+        });
+/*
+        Schema::create('passwordResetToken', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+        });
+
+        Schema::create('session', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->constrained('user')->index();
+            $table->string('ipAddress', 45)->nullable();
+            $table->text('userAgent')->nullable();
+            $table->longText('payload');
+            $table->integer('lastActivity')->index();
+        });
+*/
+    }
+
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+        //Schema::dropIfExists('passwordResetToken');
+        //Schema::dropIfExists('session');
+    }
+};
+
+
+
+/*
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Enums\UserType;
+use App\Enums\UserStatus;
+
+return new class extends Migration
+{
+
     public function up(): void
     {
         Schema::create('user', function (Blueprint $table) {
@@ -38,7 +97,6 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -51,9 +109,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('user');
@@ -61,66 +116,7 @@ return new class extends Migration
         Schema::dropIfExists('sessions');
     }
 };
+*/
 
-/*
-<?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-use App\Enums\UserType;
-use App\Enums\UserStatus;
-
-return new class extends Migration
-{
-   
-    public function up(): void
-    {
-        Schema::create('user', function (Blueprint $table) {
-            $table->id();
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('thirdName')->nullable();
-            $table->foreignId('city_id')->constrained()->onDelete('cascade');
-            $table->string('address');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('userType', array_column(UserType::cases(), 'value'))->default('Tanuló');
-
-            $table->foreignId('rfidCard_id')->constrained('rfid_cards');
-            $table->foreignId('class_id')->nullable()->constrained('classes');
-            $table->foreignId('group_id')->nullable()->constrained('groups');
-            $table->enum('status', array_column(UserStatus::cases(), 'value'))->default('inactive');
-            $table->boolean('hasDiscount')->default(false);
-            $table->timestamps();
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
-    }
-
-   
-    public function down(): void
-    {
-        Schema::dropIfExists('user');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
-    }
-};
-
-*/ 
