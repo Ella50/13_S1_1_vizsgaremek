517ea535e1d149c8e0d0cdd39211bed00da1b439
Bekebiro <farkasdominik007@gmail.com>
2025-12-02 11:41:56 +0100

    20251202_1141

--- Projekt2/frontend/src/views/Login.vue
+++ Projekt2/frontend/src/views/Login.vue
@@ -1,5 +1,24 @@
 <template>
-
+<div class="starter-page">
+    <div class="login-container">
+      <h1>Bejelentkezés</h1>
+      <form @submit.prevent="handleLogin">
+        <div class="form-group">
+          <label>Email:</label>
+          <input v-model="loginForm.email" type="email" placeholder="pelda@iskola.hu" required>
+        </div>
+        <div class="form-group">
+          <label>Jelszó:</label>
+          <input v-model="loginForm.password" type="password" placeholder="diak123" required>
+        </div>
+        <button type="submit" class="login-btn">Bejelentkezés</button>
+      </form>
+      
+      <div v-if="error" class="error">
+        {{ error }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
