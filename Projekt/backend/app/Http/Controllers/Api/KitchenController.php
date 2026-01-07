962054160191e2ebbd469075d2ff685fa91af462
Atkári Ariella <AtkariAriella@pg.local>
2026-01-07 11:03:38 +0100

    Update KitchenController.php

--- Projekt/backend/app/Http/Controllers/Api/KitchenController.php
+++ Projekt/backend/app/Http/Controllers/Api/KitchenController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Meal;
+use Log;
 use App\Enums\MealType;
 
 class KitchenController extends Controller
